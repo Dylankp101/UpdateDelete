8f9bef7342e9659d95477d1122c3f5cba5998f99
Dylan KP <60126374+Dylankp101@users.noreply.github.com>
2020-12-11 01:19:26 -0800

    added show.jsp page

--- src/main/java/com/codingdojo/upd/service/BookService.java
+++ src/main/java/com/codingdojo/upd/service/BookService.java
@@ -1,21 +1,15 @@
 package com.codingdojo.upd.service;
 
 import com.codingdojo.upd.repository.BookRepository;
-import com.sun.xml.bind.v2.model.core.ID;
-
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.codingdojo.upd.models.Book;
 @Service
 public class BookService {
-//	private final BookRepository bookRepository;
-//	public BookService(BookRepository bookRepository) {
-//		this.bookRepository = bookRepository;
-//	}
+
 	@Autowired
 	private BookRepository bookRepository;
     public List<Book> allBooks() {
@@ -25,7 +19,7 @@ public class BookService {
     public Book createBook(Book b) {
         return bookRepository.save(b);
     }
-    // creates a book
+    // deletes a book
     public Book deleteBook(Long id) {
         bookRepository.deleteById(id);
 		return null;
