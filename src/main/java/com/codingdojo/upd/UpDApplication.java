9f7ab53dfbedcb2d46a8fe532b5097a26d72697a
Dylan KP <60126374+Dylankp101@users.noreply.github.com>
2020-12-10 17:32:15 -0800

    first commit

---
+++ src/main/java/com/codingdojo/upd/UpDApplication.java
@@ -0,0 +1,13 @@
+package com.codingdojo.upd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpDApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpDApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codingdojo/upd/UpDApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.upd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpDApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
