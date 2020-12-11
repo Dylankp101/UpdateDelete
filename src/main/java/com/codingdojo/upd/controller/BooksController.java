45ac9de335e8ba5c501da8cb01e9c2a3b4096b2f
Dylan KP <60126374+Dylankp101@users.noreply.github.com>
2020-12-11 01:30:37 -0800

    added edit

--- src/main/java/com/codingdojo/upd/controller/BooksController.java
+++ src/main/java/com/codingdojo/upd/controller/BooksController.java
@@ -51,4 +51,25 @@ public class BooksController {
             return "redirect:/books";
         }
     }
+    @RequestMapping("/books/{id}/edit")
+    public String edit(@PathVariable("id") Long id, Model model) {
+        Book book = bookService.findBook(id);
+        model.addAttribute("book", book);
+        return "/edit.jsp";
+    }
+    
+    @RequestMapping(value="/books/{id}", method=RequestMethod.PUT)
+    public String update(@Valid @ModelAttribute("book") Book book, BindingResult result) {
+        if (result.hasErrors()) {
+            return "/books/edit.jsp";
+        } else {
+            bookService.createBook(book);
+            return "redirect:/books";
+        }
+    }
+    @RequestMapping(value="/books/{id}", method=RequestMethod.DELETE)
+    public String destroy(@PathVariable("id") Long id) {
+        bookService.deleteBook(id);
+        return "redirect:/books";
+    }
 }
